9af71ea05ad168328537fe2e1c9df23c2e5606ca
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-10-08 10:30:35 -0600

    further flesh out script with commands

--- sv_pipe.sh
+++ sv_pipe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=
-#SBATCH --time=04:00:00
+#SBATCH --time=05:00:00
 #SBATCH --account=marth-rw
 #SBATCH --partition=marth-shared-rw
 #SBATCH -o {path}/slurm_out/%j.out
@@ -9,32 +9,44 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=
 
-#CRAM Paths
+#Inputs to the script
 PBDCRAM=$1
 MOMCRAM=$2
 DADCRAM=$3
-DOC_VCF_OUTPUT_FILE=$4
+OG_SMOOVE_VCF=$4
 REF_FASTA=$5
 
-# set up scratch dir in scratch
+DOCTORED_MANTA_OUTPUT="doc_manta_vcf"
+DUPHOLD_MANTA_OUTPUT="dhanno_manta_vcf"
+SVAF_SMOOVE_OUTPUT="svaf_smoove_vcf"
+SVAF_MANTA_OUTPUT="svaf_manta_vcf"
+
+# set up scratch directory
 SCRDIR=/scratch/ucgd/lustre-labs/marth/scratch/$USER/$SLURM_JOB_ID
 mkdir -p $SCRDIR
+# copy the scripts and reference files to the scratch dir
+cp \
+    run_manta_trio.sh \
+    doctor_manta.py \
+    smoove.sif \
+    sv_pipe_env.yml \
+    recommended_exclusions_37.bed \
+    recommended_exclusions_38.bed \
+    SVAFotate_core_SV_popAFs.GRCh38.v4.1.bed.gz \
+    $SCRDIR
 
-# copy the scripts (not going to copy the data files themselves)
-cp run_manta_trio.sh doctor_manta.py sv_pipe_env.yml $SCRDIR
-
-# cd into that dir
 cd $SCRDIR
 
-# un the the manta script run_manta_trio.sh (needs the trio's urls CRAM format) -> new joint called vcf
+# run the the manta script run_manta_trio.sh (needs the trio's urls CRAM format) -> new joint called vcf
 ./run_manta_trio.sh $PBDCRAM $MOMCRAM $DADCRAM 
 
 # load the miniconda3 module will be needed for the doctor_manta.py and for the svafotate run at the end
 module load miniconda3/23.11.0
+# load the singularity module will be needed for the smoove duphold run
+module load singularity/4.1.1
 
+# Check if the environment already exists, if not create it
 ENV_NAME="sv_pipe_conda_env"
-
-# Check if the environment already exists
 if conda info --envs | grep -q "$ENV_NAME"; then
     conda activate "$ENV_NAME"
 else
@@ -42,16 +54,13 @@ else
     conda activate "$ENV_NAME"
 fi
 
-#the script is going to put a simlink to the manta vcf at ./diploidSV.vcf.gz
-#modify the vcf with his doctor_manta.py script (needs input vcf, and output) -> new doc_vcf
+# the script is going to put a simlink to the manta vcf at ./diploidSV.vcf.gz
+# modify the vcf with his doctor_manta.py script (needs input vcf, and output) -> new doc_vcf
+python doctor_manta.py diploidSV.vcf.gz $DOCTORED_MANTA_OUTPUT
 
-#need to have a conda environment that will have 
-
-#point to the singularity the singularity file will be in that same place
-#run DupHold on the manta vcf -> dhanno_vcf
-#per tom: user the smoove duphold module (will need to start singularity pointing to smoove docker container)
-
-#now we have a manta with duphold annotations, smoove we will already have
+# TODO: run smoove duphold on the manta vcf -> dhanno_manta_vcf
 
 #Run svafotate on both files (.8 ol threshold) -> filtered svaf_vcf
-#we will either create a conda env with smoove or activate it
+#we will either create a conda env or activate it
+svafotate annotate -v $DUPHOLD_MANTA_OUTPUT -b SVAFotate_core_SV_popAFs.GRCh38.v4.1.bed.gz -o $SVAF_MANTA_OUTPUT -f 0.8
+svafotate annotate -v $OG_SMOOVE_VCF -b SVAFotate_core_SV_popAFs.GRCh38.v4.1.bed.gz -o $SVAF_SMOOVE_OUTPUT -f 0.8
